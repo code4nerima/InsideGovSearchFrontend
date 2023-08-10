5efa57218a6754c14f9334927bc22acedd06a821
Kazuki Imamura <kaizumaki@gmail.com>
2023-08-11 07:54:53 +0900

    PandaCSSの設定をカスタマイズ

--- panda.config.ts
+++ panda.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig, defineGlobalStyles } from '@pandacss/dev'
+
+const globalCss = defineGlobalStyles({
+  'html, body': {
+    fontFamily: '"M PLUS 2 Variable", sans-serif',
+    color: 'red',
+    lineHeight: '1.55',
+  },
+})
 
 export default defineConfig({
   // Whether to use css reset
-  preflight: true,
+  preflight: false,
 
   // Where to look for your css declarations
   include: [
-    "./src/components/**/*.{ts,tsx,js,jsx}",
-    "./src/app/**/*.{ts,tsx,js,jsx}",
+    './src/components/**/*.{ts,tsx,js,jsx}',
+    './src/app/**/*.{ts,tsx,js,jsx}',
   ],
 
   // Files to exclude
@@ -16,8 +24,24 @@ export default defineConfig({
   // Useful for theme customization
   theme: {
     extend: {},
+    tokens: {
+      colors: {
+        nerimaDark: { value: '#004B38' },
+        nerimabVivid: { value: '#4DA635' },
+        nerimaLight: { value: '#CEE4AE' },
+        nerimaPale: { value: '#E7F2D9' },
+      },
+      fontWeights: {
+        thin: { value: '100' },
+        normal: { value: '400' },
+        bold: { value: '700' },
+        black: { value: '900' },
+      },
+    },
   },
 
   // The output directory for your css system
-  outdir: "styled-system",
-});
+  outdir: 'styled-system',
+
+  globalCss,
+})
