c69585b60a3c4ee9d5d5880d7f1eaf5ea211b5e2
Kazuki Imamura <kaizumaki@gmail.com>
2023-08-11 23:07:13 +0900

    exportをproductionで分岐する

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: isProd ? 'export' : undefined,
 }
 
 module.exports = nextConfig
