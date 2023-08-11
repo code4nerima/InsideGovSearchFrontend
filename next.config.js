3ce96fb135ca76a1bad14cffa504d9b3c055c51c
Kazuki Imamura <kaizumaki@gmail.com>
2023-08-12 06:12:44 +0900

    static exportをやめてみる

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: isProd ? 'export' : undefined,
+  // output: isProd ? 'export' : undefined,
 }
 
 module.exports = nextConfig
