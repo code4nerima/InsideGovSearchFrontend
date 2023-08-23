6d921702982f72dd2741d972a02966085983cb5c
Kazuki Imamura <kaizumaki@gmail.com>
2023-08-23 22:12:46 +0900

    Revert "Merge branch 'main' into add-limit"
    This reverts commit 0226906e840c3e2f5d830260da34470bb3930ca2, reversing
    changes made to 960abf75f1ccb65958a3e650e89a52e560b1cf26.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { css } from '../../styled-system/css'
 import { center, flex, grid } from '../../styled-system/patterns'
 import Loading from './loading'
@@ -43,11 +43,7 @@ export default function Home() {
     },
   ]
 
-  const [formData, setFormData] = useState({
-    currentPrompt: '',
-    selectedPrompt: '',
-    selectedLimit: 0,
-  })
+  const [currentPrompt, setCurrentPrompt] = useState('')
   const [results, setResults] = useState([])
   const [resultsGroupBy, setResultsGroupBy] = useState<Array<GroupByKeyObject>>(
     []
@@ -68,30 +64,20 @@ export default function Home() {
   const startComposition = () => setIsComposed(true)
   const endComposition = () => setIsComposed(false)
 
-  const handleInputChange = useCallback(
-    async (e: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = e.target
-      setFormData((prevState) => ({ ...prevState, [name]: value }))
-    },
-    []
-  )
-
-  const handleSearch = async (prompt: string, limit: number = 5) => {
-    if (prompt === '') return
+  const handleSearch = async (prompt: string) => {
+    if (currentPrompt === '') return
     try {
       setIsSearchExecuting(true)
       setIsResultResponded(false)
       setIsSuggestedPromptResponded(false)
       setIsAnswerResponded(false)
+      setSelectedPrompt('')
       const res = await fetch('/api/search', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-          prompt: prompt,
-          limit: Number(limit),
-        }),
+        body: JSON.stringify({ prompt: prompt, limit: selectedLimit }),
       }).then((res) => {
         if (!res.ok) {
           throw new Error(res.statusText)
@@ -107,7 +93,7 @@ export default function Home() {
       setSynonym(data.results.synonyms)
       setIsResultResponded(true)
       setIsSearchExecuting(false)
-      await suggestPrompt(prompt)
+      await suggestPrompt()
     } catch (error) {
       if (error instanceof Error) {
         console.log('Error', error.message)
@@ -120,14 +106,14 @@ export default function Home() {
     }
   }
 
-  const suggestPrompt = async (prompt: string) => {
+  const suggestPrompt = async () => {
     try {
       const res = await fetch('/api/promptVariations', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ prompt: prompt }),
+        body: JSON.stringify({ prompt: currentPrompt }),
       }).then((res) => {
         if (!res.ok) {
           throw new Error(res.statusText)
@@ -157,7 +143,7 @@ export default function Home() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          prompt: formData.currentPrompt,
+          prompt: currentPrompt,
           keywords: keyword,
           synonyms: synonym,
           answer: answer,
@@ -175,33 +161,27 @@ export default function Home() {
   const handleKeyDown = async (e: any) => {
     if (e.keyCode === 13 && !isComposed) {
       e.preventDefault()
-      await handleSearch(formData.currentPrompt, formData.selectedLimit)
+      await handleSearch(currentPrompt)
     }
   }
 
   const handleReset = () => {
-    setFormData((prevState) => ({
-      ...prevState,
-      currentPrompt: '',
-      selectedPrompt: '',
-    }))
+    setCurrentPrompt('')
     setResults([])
     setResultsGroupBy([])
     setKeyword('')
     setSynonym('')
     setSuggestedPrompts([])
+    setSelectedPrompt('')
     setIsResultResponded(false)
     setIsSuggestedPromptResponded(false)
     setIsAnswerResponded(false)
   }
 
   const handleChangePrompt = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      currentPrompt: e.target.value,
-      selectedPrompt: e.target.value,
-    }))
-    await handleSearch(e.target.value, formData.selectedLimit)
+    setSelectedPrompt(e.target.value)
+    setCurrentPrompt(e.target.value)
+    await handleSearch(e.target.value)
   }
 
   useEffect(() => {
@@ -347,7 +327,6 @@ export default function Home() {
                 <input
                   id="prompt"
                   type="search"
-                  name="currentPrompt"
                   className={css({
                     appearance: 'none',
                     borderRadius: '3em',
@@ -366,12 +345,12 @@ export default function Home() {
                       cursor: 'wait',
                     },
                   })}
-                  value={formData.currentPrompt}
+                  value={currentPrompt}
                   placeholder="例）引っ越ししたときの手続きをしたい"
                   disabled={isSearchExecuting}
                   onCompositionStart={startComposition}
                   onCompositionEnd={endComposition}
-                  onChange={handleInputChange}
+                  onChange={(e) => setCurrentPrompt(e.target.value)}
                   onKeyDown={handleKeyDown}
                 />
                 <button
@@ -406,9 +385,7 @@ export default function Home() {
                     },
                   })}
                   disabled={isSearchExecuting}
-                  onClick={() =>
-                    handleSearch(formData.currentPrompt, formData.selectedLimit)
-                  }
+                  onClick={() => handleSearch(currentPrompt)}
                 >
                   検索
                 </button>
@@ -429,12 +406,10 @@ export default function Home() {
                         },
                       })}
                       id={`limit-select-id-${i}`}
-                      name="selectedLimit"
+                      name="limit-select"
                       value={option.value}
-                      checked={
-                        String(formData.selectedLimit) === String(option.value)
-                      }
-                      onChange={handleInputChange}
+                      checked={selectedLimit === option.value}
+                      onChange={() => setSelectedLimit(option.value)}
                     />
                     <label
                       htmlFor={`limit-select-id-${i}`}
@@ -465,7 +440,7 @@ export default function Home() {
                 disabled={
                   isSearchExecuting ||
                   (!isSuggestedPromptResponded && isResultResponded) ||
-                  (formData.currentPrompt === '' && !isResultResponded)
+                  (currentPrompt === '' && !isResultResponded)
                 }
                 onClick={handleReset}
               >
@@ -557,7 +532,7 @@ export default function Home() {
                           id={`suggested-prompt-id-${i}`}
                           name="suggested-prompt"
                           value={prompt}
-                          checked={formData.selectedPrompt === prompt}
+                          checked={selectedPrompt === prompt}
                           onChange={handleChangePrompt}
                         />
                         <label
