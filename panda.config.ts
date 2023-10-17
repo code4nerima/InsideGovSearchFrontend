9a2afbb9cdd3065b2cc83a49c72b3230bfc1e624
Kazuki Imamura <kaizumaki@gmail.com>
2023-10-17 17:42:49 +0900

    supportsをglobals.cssに記述

--- panda.config.ts
+++ panda.config.ts
@@ -103,12 +103,6 @@ export default defineConfig({
     },
   },
 
-  conditions: {
-    extend: {
-      supportTouchCalloutNone: '@supports (-webkit-touch-callout: none)',
-    },
-  },
-
   // The output directory for your css system
   outdir: 'styled-system',
 
